f7fcc0485a924b7d67a283f246c0b8dcd5ff6e77
alpha clash pro <mdsadmansakib6163327@gmail.com>
2024-12-05 01:24:36 +0600

    learning

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 
@@ -30,152 +31,136 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const doctorProfile = client.db('DoctorInfo').collection('users');
-    const doctorService = client.db('DoctorInfo').collection('service');
-    const bookinsProfile = client.db('bookings').collection('books');
+    const usersProfile = client.db('DoctorInfo').collection('users');
+    const doctorService = client.db('DoctorInfo').collection('doctor');
+    const bookinsProfile = client.db('DoctorInfo').collection('books');
 
-
-
-    app.get('/users', async (req, res) => {
-      const cursor = doctorProfile.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-    app.get('/users/:id', async (req, res) => {
-      const userId = req.params.id;
-      const query = { _id: new ObjectId(userId) };
-      const options = {
-        projection: {
-          name: 1,
-          credentials: 1,
-          rating: 1,
-          location: 1,
-          specialties: 1,
-          about: 1,
-          education: 1,
-          awards: 1,
-          services: 1,
-          specializations: 1,
-          businessHours: 1,
-          image: 1,
-          calendar: 1,
-          title: 1,
-        },
-      };
-      const result = await doctorProfile.findOne(query, options);
-      res.send(result);
-    })
-    app.post('/users', async (req, res) => {
-      const { name, email, specialty, photo } = req.body;
-    
-      const newDoctorProfile = {
-        name,
-        email,
-        specialty,
-        photo,
-      };
-      
-      // Insert the new doctor profile into the collection
-      const result = await doctorProfile.insertOne(newDoctorProfile);
-    
-      if (result.acknowledged) {
-        res.status(201).json({
-          message: 'Doctor profile added successfully',
-          doctorProfile: newDoctorProfile,
-        });
-      } else {
-        res.status(400).json({
-          message: 'Failed to add doctor profile',
-        });
-      }
-    });
-
-
-
-    // book
-
-
-    app.post('/books', async (req, res) => {
-      try {
-        const user = req.body; // Data from the frontend form submission
-        if (!user) {
-          return res.status(400).send({ message: 'No data provided' });
-        }
-
-        // Insert the user data (booking information) into the 'books' collection
-        const response = await bookinsProfile.insertOne(user);
-
-        // Send back the result
-        res.send(response);
-      } catch (error) {
-        console.error('Error inserting booking:', error);
-        res.status(500).send({
-          message: 'Error inserting booking',
-          error: error.message
-        });
-      }
-    });
-
-
-
-
-
-//service    
-
-
-    app.get('/service', async (req, res) => {
-      const cursor = doctorService.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-
-    app.get('/service/:id', async (req, res) => {
-      const userId = req.params.id;
-      const query = { _id: new ObjectId(userId) };
-      const options = {
-        projection: {
-          name: 1,
-          service: 1,
-          time: 1,
-        },
-      };
-      const result = await doctorService.findOne(query, options);
-      res.send(result);
-    })
-
-    app.post('/service', async (req, res) => {
+    // jwt releted api
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
+      const token = jwt.sign(user, process.env.DB_TOKEN, { expiresIn: '1h' });
+      res.send({ token })
+    })
 
-      doctorService.insertOne(user)
-        .then((response) => {
-          res.status(201).send({
-            success: true,
-            message: "Service added successfully",
-            data: response
-          });
-        })
-        .catch((error) => {
-          res.status(500).send({
-            success: false,
-            message: "Failed to add service",
-            error: error.message
-          });
-        });
-    });
 
-    app.delete('/service/:id', async(req, res ) => {
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside the token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.DB_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+      })    
+    }
+
+    const veryfiedAdmin = async(req,res,next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersProfile.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next();
+    }
+    // user releted api
+    app.get('/users', verifyToken, async (req, res) => {
+      const result = await usersProfile.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/users/admin/:email', verifyToken,veryfiedAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email };
+      const user = await usersProfile.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.post('/users', verifyToken, veryfiedAdmin, async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email }
+      const existingUser = await usersProfile.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
+      }
+      const result = await usersProfile.insertOne(user);
+      res.send(result);
+    })
+
+    app.delete('/users/:id',verifyToken, veryfiedAdmin, async (req, res) => {
       const user = req.params.id;
-      console.log(user)
-      const query = {_id: new ObjectId(user) }
-      const responce = await doctorService.deleteOne(query);
-      res.send(responce);
+      const query = { _id: new ObjectId(user) }
+      const result = await usersProfile.deleteOne(query);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id',verifyToken,veryfiedAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersProfile.updateOne(filter, updateDoc);
+      res.send(result);
+
     })
 
 
 
-    /// user
-   
+    ///doctor about releted api
+    app.get('/doctor', async (req, res) => {
+      const cursor = await doctorService.find().toArray();
+      res.send(cursor);
+    })
+    app.get('/doctor/:id', async (req, res) => {
+      const user = req.params.id;
+      const query = { _id: new ObjectId(user) }
+      const result = await doctorService.findOne(query);
+      res.send(result);
+
+    })
+    
+
+    // appointment releted api
+    app.post('/books', async (req, res) => {
+      const user = req.body;
+      const result = await bookinsProfile.insertOne(user);
+      res.send(result)
+    })
+
+    // manage 
+    app.get('/menage', verifyToken, veryfiedAdmin, async (req, res) => {
+      try {
+          const cursor = await doctorService.find().toArray(); // Retrieve all doctor data
+          res.send(cursor); // Send the data back to the client
+      } catch (error) {
+          console.error('Error fetching doctors:', error);
+          res.status(500).send({ error: 'Failed to fetch doctor data' });
+      }
+  });
+  
+  
+    app.post('/manage',verifyToken, veryfiedAdmin, async(req,res) => {
+      const item = req.body;
+      const result = await doctorService.insertOne(item)
+      res.send(result);
+    })
+
 
 
 
